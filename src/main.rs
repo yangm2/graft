d1edbae12512e704eb9d9313089899c1f60ec14b
yangm2 <yangm2@users.noreply.github.com>
2021-09-08 20:11:41 -0700

    utilize ClapV3 and clean up error handling

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,13 @@
-const USAGE: &str = "
-Usage:
-    graft {PATH}";
-
-use std::env;
+use clap::{App, Arg};
 use std::error;
 use std::error::Error;
 use std::fmt;
+use std::path::Path;
 
-// We derive `Debug` because all types should probably derive `Debug`.
-// This gives us a reasonable human-readable description of `CliError` values.
-#[derive(Debug)]
+// Custom error type
 enum CliError {
-    MissingArg,
-    NotDir,
+    DstDirNotEmpty(String),
+    NotDir(String),
     WrongArgs,
 }
 
@@ -23,49 +18,59 @@ impl error::Error for CliError {
 }
 
 impl fmt::Display for CliError {
+    fn fmt(&self, _f: &mut fmt::Formatter) -> fmt::Result {
+        unimplemented!()
+    }
+}
+
+impl fmt::Debug for CliError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
+        match self {
             // Both underlying errors already impl `Display`, so we defer to
             // their implementations.
-            CliError::MissingArg => write!(f, "Missing Arg:"),
-            CliError::NotDir => write!(f, "Not a Directory:"),
+            CliError::DstDirNotEmpty(s) => write!(f, "Destination directory ({}) is not empty", s),
+            CliError::NotDir(s) => write!(f, "Not a Directory: {}", s),
             CliError::WrongArgs => write!(f, "Wrong number of args"),
         }
     }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    use std::ffi::OsString;
-    use std::path::Path;
-
-    fn parse_arg(mut argv: env::ArgsOs) -> Result<OsString, CliError> {
-        fn check(p: OsString) -> Result<OsString, CliError> {
+    fn parse_arg_with_clap() -> Result<String, CliError> {
+        fn check(p: &str) -> Result<String, CliError> {
             // TODO: is the dir Readable?  eXecutable?
-            if Path::new(&p.clone()).is_dir() {
-                Ok(p)
-            } else {
-                Err(CliError::NotDir)
+            match Path::new(&p).is_dir() {
+                true => Ok(p.to_string()),
+                false => Err(CliError::NotDir(p.to_string())),
             }
         }
 
-        let dir = argv
-            .by_ref()
-            .nth(1)
-            .ok_or(CliError::MissingArg)
-            .and_then(check);
+        let matches = App::new("graft")
+            .version("0.2")
+            .author("Mike")
+            .about("Replicate the structure of an original directory-tree and populate the replica with symlinks to the original directory-tree.")
+            .arg(Arg::new("DIR")
+                .about("source directory path")
+                .required(true)
+                .index(1)
+            )
+            .get_matches();
 
-        if argv.count() != 0 {
-            print!("{}\n\n", USAGE);
-            return Err(CliError::WrongArgs);
-        };
+        // You can check the value provided by positional arguments, or option arguments
+        if let Some(i) = matches.value_of("DIR") {
+            println!("Value for DIR: {}", i);
+        }
 
-        dir
+        matches
+            .value_of("DIR")
+            .ok_or(CliError::WrongArgs)
+            .and_then(check)
     }
 
-    // 1. create subdirectory tree
-    // 2. if not symlink, create symlink
-    // 3. if symlink, copy symlink (both absolute and relative)
-
+    // Algorithm:
+    //   1. create subdirectory tree
+    //   2. if not symlink, create symlink
+    //   3. if symlink, copy symlink (both absolute and relative)
     fn recurse(srcdir: &Path, cwd: &Path) -> std::io::Result<()> {
         use std::fs;
         use std::os::unix::fs as unix_fs;
@@ -97,29 +102,40 @@ fn main() -> Result<(), Box<dyn Error>> {
         Ok(())
     }
 
-    let dstdir = Path::new(".");
-
-    fn dir_is_empty(dst: &Path) -> Result<bool, bool> {
+    fn dir_is_empty(dst: &Path) -> Result<bool, CliError> {
         match dst.read_dir() {
             Ok(entry) => {
-                if entry.count() != 0 {
-                    eprintln!("Directory not empty!!!");
-                    Err(false)
+                if entry.count() > 0 {
+                    Ok(false)
                 } else {
                     Ok(true)
                 }
             }
-            Err(_e) => Err(false),
+            Err(_e) => Err(CliError::NotDir(
+                dst.canonicalize().unwrap().display().to_string(),
+            )),
         }
     }
 
-    // error unless current dir (i.e. target) is empty
-    assert_eq!(dir_is_empty(&dstdir), Ok(true));
-
-    match parse_arg(env::args_os()) {
+    match parse_arg_with_clap() {
         Ok(srcdir) => {
+            // validate the destination directory
+            let dstdir = Path::new(".");
+            // error unless current dir (i.e. target) is empty
+            if let Ok(empty) = dir_is_empty(&dstdir) {
+                if !empty {
+                    return Err(Box::from(CliError::DstDirNotEmpty(
+                        dstdir.canonicalize()?.display().to_string(),
+                    )));
+                }
+            }
+
             let dir = Path::new(&srcdir);
-            recurse(&dir, &dstdir).or_else(|_| Err(Box::from(CliError::NotDir)))
+            recurse(&dir, &dstdir).or_else(|_| {
+                Err(Box::from(CliError::NotDir(
+                    dir.canonicalize()?.display().to_string(),
+                )))
+            })
         }
         Err(e) => Err(Box::from(e)),
     }
